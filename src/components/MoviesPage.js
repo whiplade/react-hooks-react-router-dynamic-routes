98b3ddd4623abba30d166b2b1300e4603fb18594
whiplade <nmawira13@gmail.com>
2023-07-26 13:42:55 +0300

    Completed Assignment

--- src/components/MoviesPage.js
+++ src/components/MoviesPage.js
@@ -1,11 +1,21 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, useRouteMatch } from "react-router-dom";
 import MoviesList from "./MoviesList";
+import MovieShow from "./MoviesShow";
 
 function MoviesPage({ movies }) {
+
+const match = useRouteMatch()
+
   return (
     <div>
       <MoviesList movies={movies} />
+      <Route exact path={`${match.url}`}>
+        <h3>Choose a movie from the list above</h3>
+      </Route>
+      <Route path={`${match.url}/:movieID`}>
+        <MovieShow movies={movies}/>
+      </Route>
     </div>
   );
 }
